c6086037ac1caa95a2395018a7c37e255a4ae1c2
SangSepuhYangMerendahSampaiIntiBumi <151481346+SangSepuhYangMerendahSampaiIntiBumi@users.noreply.github.com>
2023-11-23 14:57:00 +0700

    Add files via upload

--- ProjekHotel/Petugas-1.php
+++ ProjekHotel/Petugas-1.php
@@ -3,13 +3,6 @@
 <head>
 <link rel="stylesheet" href="style1.css">
 <style>
-body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-}
 </style>
     <div class>
     <header>
@@ -19,19 +12,19 @@ body {
                 </div>
                 <nav>       
                     <ul>
-                        <li><a href="http://localhost/hotel.r/index.html">Home</a></li>
-                        <li><a href="http://localhost/hotel.r/index.html#about">About</a></li>
-                        <li><a href="http://localhost/hotel.r/index.html#room-list">Galery</a></li>
-                        <li><a href="http://localhost/hotel.r/index.html#contatc">Contatc Us</a></li>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index.html#about">About</a></li>
+                        <li><a href="index.html#room-list">Galery</a></li>
+                        <li><a href="index.html#contatc">Contatc Us</a></li>
                     </ul>
                 </nav>  
                 <div class="dropdown">
                     <a class="dropdown-item">Admin</a>
                     <div class="dropdown-isi">
-                        <a href="http://localhost/hotel.r/tamu.php">Tamu</a>
-                        <a href="http://localhost/hotel.r/kamar.php">Kamar</a>
-                        <a href="http://localhost/hotel.r/reservasi.php">Reservasi</a>
-                        <a href="http://localhost/hotel.r/karyawan.php">Karyawan</a>
+                        <a href="Pengunjung.php">Tamu</a>
+                        <a href="kamar.php">Kamar</a>
+                        <a href="adminidtrasi.php">Reservasi</a>
+                        <a href="Petugas-1.php">Karyawan</a>
                     </div>
                 </div>
                 <div class="button-container">
--- ProjekHotel/adminidtrasi.php
+++ ProjekHotel/adminidtrasi.php
@@ -12,19 +12,19 @@
                 </div>
                 <nav>       
                     <ul>
-                        <li><a href="http://localhost/hotel.r/index.html">Home</a></li>
-                        <li><a href="http://localhost/hotel.r/index.html#about">About</a></li>
-                        <li><a href="http://localhost/hotel.r/index.html#room-list">Galery</a></li>
-                        <li><a href="http://localhost/hotel.r/index.html#contatc">Contatc Us</a></li>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index.html#about">About</a></li>
+                        <li><a href="index.html#room-list">Galery</a></li>
+                        <li><a href="index.html#contatc">Contatc Us</a></li>
                     </ul>
                 </nav>  
                 <div class="dropdown">
                     <a class="dropdown-item">Admin</a>
                     <div class="dropdown-isi">
-                        <a href="http://localhost/hotel.r/tamu.php">Tamu</a>
-                        <a href="http://localhost/hotel.r/kamar.php">Kamar</a>
-                        <a href="http://localhost/hotel.r/reservasi.php">Reservasi</a>
-                        <a href="http://localhost/hotel.r/karyawan.php">Karyawan</a>
+                        <a href="Pengunjung.php">Tamu</a>
+                        <a href="kamar.php">Kamar</a>
+                        <a href="adminidtrasi.php">Reservasi</a>
+                        <a href="Petugas-1.php">Karyawan</a>
                     </div>
                 </div>
                 <div class="button-container">
